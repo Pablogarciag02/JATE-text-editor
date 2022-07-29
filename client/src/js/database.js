dc3aa32a2242134c1b6302b61c2248c327e67f44
Pablo Garcia <pgarciag566@gmail.com>
2022-07-29 14:07:58 -0500

    added logic for accept(put) and get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Add to db");
+  const connectDb = await openDB("jate", 1);
+  const tx = connectDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const result = await store.put({id: 1, content: content});
+  console.log("Saved", result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("Get from db");
+  const connectDb = await openDB("jate", 1);
+  const tx = connectDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const result = await store.get(1);
+  console.log("Result", result);
+  return result;
+}
 
 initdb();
